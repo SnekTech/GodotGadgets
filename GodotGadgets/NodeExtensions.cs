dfd9204213472db59fc312b824d3077c2dc4ae4f
syf <2654525303@qq.com>
2025-07-18 22:21:11 +0800

    init repo
    - add node extension for clearing children nodes

---
+++ GodotGadgets/NodeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace GodotGadgets;
+
+public static class NodeExtensions
+{
+    public static void ClearChildren(this Node node)
+    {
+        foreach (var child in node.GetChildren())
+        {
+            child.QueueFree();
+        }
+    }
+}
