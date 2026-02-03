81ddcb384fc39946528b7aba0e4ccc7527e1eb55
syf <2654525303@qq.com>
2026-02-03 21:29:43 +0800

    add extension to create uniform from shader material

--- GodotGadgets/ShaderStuff/Uniform.cs
+++ GodotGadgets/ShaderStuff/Uniform.cs
@@ -1,6 +1,4 @@
-﻿using JetBrains.Annotations;
-
-namespace GodotGadgets.ShaderStuff;
+﻿namespace GodotGadgets.ShaderStuff;
 
 [UsedImplicitly]
 public class Uniform<[MustBeVariant] T>(ShaderMaterial shaderMaterial, StringName name) where T : struct
@@ -14,5 +12,14 @@ public class Uniform<[MustBeVariant] T>(ShaderMaterial shaderMaterial, StringNam
             field = value;
             shaderMaterial.SetShaderParameter(name, Variant.From(value));
         }
+    }
+}
+
+public static class UniformExtensions
+{
+    extension(ShaderMaterial shaderMaterial)
+    {
+        public Uniform<T> GetUniform<[MustBeVariant] T>(StringName name) where T : struct =>
+            new(shaderMaterial, name);
     }
 }
