53ed000ef18e30b043706270a3634b1582ed626c
syf <2654525303@qq.com>
2026-03-05 10:57:41 +0800

    uniform extensions for tweening

--- GodotGadgets/ShaderStuff/Uniform.cs
+++ GodotGadgets/ShaderStuff/Uniform.cs
@@ -1,4 +1,9 @@
-﻿namespace GodotGadgets.ShaderStuff;
+﻿using GTweens.Extensions;
+using GTweens.Tweeners;
+using GTweens.Tweens;
+using GTweensGodot.Extensions;
+
+namespace GodotGadgets.ShaderStuff;
 
 [UsedImplicitly]
 public class Uniform<[MustBeVariant] T>(ShaderMaterial shaderMaterial, StringName name) where T : struct
@@ -19,7 +24,35 @@ public static class UniformExtensions
 {
     extension(ShaderMaterial shaderMaterial)
     {
+        [UsedImplicitly]
         public Uniform<T> GetUniform<[MustBeVariant] T>(StringName name) where T : struct =>
             new(shaderMaterial, name);
+    }
+
+    extension<[MustBeVariant] T>(Uniform<T> uniform) where T : struct
+    {
+        Tweener<T>.Getter Getter => () => uniform.Value;
+        Tweener<T>.Setter Setter => v => uniform.Value = v;
+    }
+
+    extension(Uniform<float> uniform)
+    {
+        [UsedImplicitly]
+        public GTween Tween(float to, float duration)
+            => GTweenExtensions.Tween(uniform.Getter, uniform.Setter, to, duration);
+    }
+
+    extension(Uniform<Vector2> uniform)
+    {
+        [UsedImplicitly]
+        public GTween Tween(Vector2 to, float duration) =>
+            GTweenGodotExtensions.Tween(uniform.Getter, uniform.Setter, to, duration);
+    }
+
+    extension(Uniform<Color> uniform)
+    {
+        [UsedImplicitly]
+        public GTween Tween(Color to, float duration) =>
+            GTweenGodotExtensions.Tween(uniform.Getter, uniform.Setter, to, duration);
     }
 }
