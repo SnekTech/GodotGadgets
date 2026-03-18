721b17f226197ebdbd636a5ca43f2446b899c93d
syf <2654525303@qq.com>
2026-03-18 21:48:13 +0800

    add CancellableTweenHolder

---
+++ GodotGadgets/TweenStuff/CancellableTweenHolder.cs
@@ -0,0 +1,24 @@
+﻿using GodotGadgets.Tasks;
+using GTweens.Tweens;
+using GTweensGodot.Extensions;
+
+namespace GodotGadgets.TweenStuff;
+
+[UsedImplicitly]
+public sealed class CancellableTweenHolder : IDisposable
+{
+    CancellationTokenSource _ctsPrevious = new();
+
+    public Task CancelPreviousAndPlayAsync(GTween newTween, CancellationToken ct = default)
+    {
+        _ctsPrevious.CancelAndDispose();
+        _ctsPrevious = new CancellationTokenSource();
+
+        return newTween.PlayAsync(ct.LinkTo(_ctsPrevious.Token).Token);
+    }
+
+    public void Dispose()
+    {
+        _ctsPrevious.Dispose();
+    }
+}
