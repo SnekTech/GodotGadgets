283876f792da9f00843cb7fdd7404929a2e0f4ff
syf <2654525303@qq.com>
2026-05-04 15:41:57 +0800

    add SingleTweenPlayer

---
+++ GodotGadgets/TweenStuff/SingleTweenPlayer.cs
@@ -0,0 +1,22 @@
+﻿using GTweens.Tweens;
+using GTweensGodot.Extensions;
+
+namespace GodotGadgets.TweenStuff;
+
+[UsedImplicitly]
+public sealed class SingleTweenPlayer : IDisposable
+{
+    GTween? _currentTween;
+
+    public void KillPreviousAndPlay(GTween newTween)
+    {
+        _currentTween?.Kill();
+        _currentTween = newTween;
+        newTween.Play();
+    }
+    
+    public void Dispose()
+    {
+        _currentTween?.Kill();
+    }
+}
