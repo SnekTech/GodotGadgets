92c6ae5f9aa977aedc4c4b42d90b2183dd907150
syf <2654525303@qq.com>
2026-06-25 11:03:43 +0800

    tool class for button handler registration

---
+++ GodotGadgets/UI/ButtonBinding.cs
@@ -0,0 +1,34 @@
+﻿namespace GodotGadgets.UI;
+
+public readonly record struct ButtonAndHandler(Button Button, Action Handler);
+
+public sealed class ButtonBindings : IDisposable
+{
+    readonly ButtonAndHandler[] _bindings;
+
+    public ButtonBindings(params ButtonAndHandler[] bindings)
+    {
+        _bindings = bindings.ToArray();
+
+        foreach (var binding in _bindings)
+        {
+            binding.Button.Pressed += binding.Handler;
+        }
+    }
+
+    public void Dispose()
+    {
+        foreach (var binding in _bindings)
+        {
+            binding.Button.Pressed -= binding.Handler;
+        }
+    }
+}
+
+public static class ButtonHandlerExtensions
+{
+    extension(Button button)
+    {
+        public ButtonAndHandler BindHandler(Action handler) => new(button, handler);
+    }
+}
