624541b146c272e7cf936257ac625a1b6cb3fddb
syf <2654525303@qq.com>
2026-07-16 21:30:14 +0800

    add extension method for page slicing

---
+++ GodotGadgets/UI/Pagination/PageQueryExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace GodotGadgets.UI.Pagination;
+
+public static class PageQueryExtensions
+{
+    extension<T>(IReadOnlyList<T> source)
+    {
+        public PageResult<T> SlicePage(PageRequest request)
+        {
+            var (pageIndex, pageSize) = request;
+            var itemsInPage = source
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToArray();
+            return new PageResult<T>(itemsInPage, source.Count);
+        }
+    }
+
+    extension<T>(IEnumerable<T> source)
+    {
+        public PageResult<T> SlicePage(PageRequest request)
+        {
+            var materialized = source.ToArray();
+            return materialized.SlicePage(request);
+        }
+    }
+}
